9459068a256f2b27548eca715af8d5a00a8dbcce
Daryl George <Daryl George@example.com>
2019-06-02 22:23:00 +1200

    GitAndroidStudio first commit

---
+++ app/src/main/java/george/daryl646/gitandroidstudio/MainActivity.java
@@ -0,0 +1,13 @@
+package george.daryl646.gitandroidstudio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
